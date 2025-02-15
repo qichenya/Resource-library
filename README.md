a700aa6c4b93363281bc15ad9760661623bd9a56
qichenya <qichengongzuoshi@gmail.com>
2025-02-15 08:45:03 +0800

    update README.md,add download link button on category's window by 4C01, add basic example resource detail by qichenya

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ This is a website for listing resource and classing resource.
 The Basic usage is collect programs and softwares into a static website.  
   
 this is the feature that qichenya's Resource Library able to do:  
-## collect infomation
-collect the website or the link address and anything you want, just like the feature: "add anything to your favorite"
+## collect software link
+collect the website or the link address and anything you want, just like the feature: "add it to your favorite"
 ## searching
 search the specified of infomation, or search by name to find out what you collected.
 ## add discription
-if you forgot something's usage or why you save it in your Library,you can easily findout it in the discription
+if you forgot the specified software's usage or why you save it in your Library,you can easily findout it in the discription
--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@
 
 const space = "&nbsp&nbsp&nbsp&nbsp&nbsp";
 const resources = [
-    { id: 1, name: 'Minecraft', category: 'game', description: '一款沙盒游戏，支持多人在线', downloadLink: 'https://expover.iiu/minecraft', iconLink: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/logos/Homepage_Gameplay-Trailer_MC-OV-logo_300x300.png' },
-    { id: 2, name: 'Photoshop', category: 'software', description: '图像编辑软件，功能强大', downloadLink: 'https://expover.iiu/photoshop', iconLink: 'https://helpx.adobe.com/content/dam/help/mnemonics/ps_cc_app_RGB.svg' },
-    { id: 3, name: 'Notepad++', category: 'tool', description: '一个开源文本编辑器，适用于多种编程语言', downloadLink: 'https://expover.iiu/notepad', iconLink: 'https://notepad.org.cn/zb_users/upload/2025/01/202501281738050571206544.png' },
-    { id: 4, name: 'The Witcher 3', category: 'game', description: '一款开放世界的角色扮演游戏', downloadLink: 'https://expover.iiu/witcher3', iconLink: 'https://bkimg.cdn.bcebos.com/pic/d058ccbf6c81800a3f2cc205b63533fa838b47eb?x-bce-process=image/format,f_auto/quality,Q_70/resize,m_lfit,limit_1,w_536' },
+    { id: 1, name: 'Minecraft', category: 'game', description: '一款沙盒游戏，支持多人在线', downloadLink: 'https://expover.iiu/minecraft', officialLink: "", iconLink: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/logos/Homepage_Gameplay-Trailer_MC-OV-logo_300x300.png' },
+    { id: 2, name: 'Photoshop', category: 'software', description: '图像编辑软件，功能强大', downloadLink: 'https://expover.iiu/photoshop', officialLink: "", iconLink: 'https://helpx.adobe.com/content/dam/help/mnemonics/ps_cc_app_RGB.svg' },
+    { id: 3, name: 'Notepad++', category: 'tool', description: '一个开源文本编辑器，适用于多种编程语言', downloadLink: 'https://expover.iiu/notepad', officialLink: "", iconLink: 'https://notepad.org.cn/zb_users/upload/2025/01/202501281738050571206544.png' },
+    { id: 4, name: 'The Witcher 3', category: 'game', description: '一款开放世界的角色扮演游戏', downloadLink: 'https://expover.iiu/witcher3', officialLink: "", iconLink: 'https://bkimg.cdn.bcebos.com/pic/d058ccbf6c81800a3f2cc205b63533fa838b47eb?x-bce-process=image/format,f_auto/quality,Q_70/resize,m_lfit,limit_1,w_536' },
     // 更多资源...
 ];
 
@@ -20,7 +20,7 @@ function loadResources() {
         var imagesrc = "";
         if(resource.iconLink.length > 0){imagesrc = resource.iconLink}
         else{imagesrc = "image/nullicon.png"}
-        div.innerHTML = `<a href="resource.html?id=${resource.id}"><div id="content_pic"><img src="${imagesrc}" alt="${resource.name}" width=100px height=100px></div><div id="content_info"><h1>${resource.name}</h1></p>${space}${resource.description} </div></a>`;
+        div.innerHTML = `<a href="resource.html?id=${resource.id}"><div id="content_pic"><img src="${imagesrc}" alt="${resource.name}" width=100px height=100px></div><div id="content_info"><h1>${resource.name}</h1></p>${space}${resource.description} </div></a><div id=“linkbox”><a href="${resource.downloadLink}"><div id="download"><h2>点击下载</h2></div></a><a href="${resource.officialLink}"><div id="download"><h2>前往官网</h2></div></a>`;
         resourcesList.appendChild(div);
     });
 }
@@ -39,7 +39,7 @@ function loadCategoryResources() {
         var imagesrc = "";
         if(resource.iconLink.length > 0){imagesrc = resource.iconLink}
         else{imagesrc = "image/nullicon.png"}
-        div.innerHTML = `<a href="resource.html?id=${resource.id}"><div id="content_pic"><img src="${imagesrc}" alt="${resource.name}" width=100px height=100px></div><div id="content_info"><h1>${resource.name}</h1></p>${space}${resource.description} </div></a>`;
+        div.innerHTML = `<a href="resource.html?id=${resource.id}"><div id="content_pic"><img src="${imagesrc}" alt="${resource.name}" width=100px height=100px></div><div id="content_info"><h1>${resource.name}</h1></p>${space}${resource.description} </div></a><div id=“linkbox”><a href="${resource.downloadLink}"><div id="download"><h2>点击下载</h2></div></a><a href="${resource.officialLink}"><div id="download"><h2>前往官网</h2></div></a>`;
         resourcesList.appendChild(div);
     });
 }
@@ -64,7 +64,7 @@ function searchResources(categoryfilter) {
         var imagesrc = "";
         if(resource.iconLink.length > 0){imagesrc = resource.iconLink}
         else{imagesrc = "image/nullicon.png"}
-        div.innerHTML = `<a href="resource.html?id=${resource.id}"><div id="content_pic"><img src="${imagesrc}" alt="${resource.name}" width=100px height=100px></div><div id="content_info"><h1>${resource.name}</h1></p>${space}${resource.description} </div></a>`;
+        div.innerHTML = `<a href="resource.html?id=${resource.id}"><div id="content_pic"><img src="${imagesrc}" alt="${resource.name}" width=100px height=100px></div><div id="content_info"><h1>${resource.name}</h1></p>${space}${resource.description} </div></a><div id=“linkbox”><a href="${resource.downloadLink}"><div id="download"><h2>点击下载</h2></div></a><a href="${resource.downloadLink}"><div id="download"><h2>前往官网</h2></div></a>`;
         resourcesList.appendChild(div);
     });
 }
