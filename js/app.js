7201d1c604c303543e8352bb1c92a066d8778861
qichenya <qichengongzuoshi@gmail.com>
2025-02-16 18:09:40 +0800

    fix: 优化了mata.json 的用途,将mata.json 替代了resource.html

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 // 静态资源数据
 
-const space = "&nbsp&nbsp&nbsp&nbsp&nbsp";
-const resources = [
-    { id: 1, name: "Blibili", description: "bilibili是国内知名的视频弹幕网站", downloadLink: "https://app.bilibili.com/", officialLink: "https://www.bilibili.com/", iconLink: "https://th.bing.com/th/id/R.5f7657549f4bd16d08b56ccad794f01a?rik=%2bXBzOkKWIBMU8A&riu=http%3a%2f%2fwww.kuaipng.com%2fUploads%2fpic%2fw%2f2020%2f07-20%2f89190%2fwater_89190_698_698_.png&ehk=b6FaBHz%2bTBstG7fZg%2fGSVXCcCSqOw3pUcMIZr6QUdZg%3d&risl=&pid=ImgRaw&r=0" },
-    { id: 2, name: "Ollama", description: "Get up and running with large language models.", downloadLink: "/detail.html?key=ollama", officialLink: "https://ollama.com/", iconLink: "https://ollama.com/public/ollama.png" },
-    { id: 3, name: "EncryptSynaptics", description: "针对于Synaptics蠕虫的杀毒工具", downloadLink: "/detail.html?key=encrypt-synaptics", officialLink: "/ruanjian/null.html", iconLink: "" },
-    { id: 4, name: "Clash Verge", description: "基于 Tauri 的现代 GUI 客户端，以提供...", downloadLink: "/detail.html?key=clash-verge", officialLink: "https://clashcn.com/clash-verge", iconLink: "https://clashcn.com/wp-content/uploads/2023/11/clashverge-300x300-1.png" },
-];
+const space = "";
+let resources = [];
 
+async function getJSON() {
+    const response = await fetch("meta.json");
+    const resourcesinput= await response.json();
+    resources = Array.from(resourcesinput.resources);
+    console.warn(resources);
+}
 // 动态加载资源到首页
 function loadResources() {
     const resourcesList = document.getElementById("resources-list");
@@ -22,7 +23,8 @@ function loadResources() {
         } else {
             imagesrc = "image/nullicon.png";
         }
-        div.innerHTML = `<a href="resource.html?id=${resource.id}"><div id="content_pic"><img src="${imagesrc}" alt="${resource.name}" width=100px height=100px></div><div id="content_info"><h1>${resource.name}</h1></p>${space}${resource.description} </div></a><div id=“linkbox”><a href="${resource.downloadLink}"><div id="download"><h2>点击下载</h2></div></a><a href="${resource.officialLink}"><div id="download"><h2>前往官网</h2></div></a>`;
+        if (!resource.website){resource.website = "/null.html"}
+        div.innerHTML = `<a href="resource.html?id=${resource.id}" target="_blank"><div id="content_pic"><img src="${imagesrc}" alt="${resource.displayName}" width=100px height=100px></div><div id="content_info"><h1>${resource.displayName}</h1></p>${space}${resource.detailTitle} </div></a><div id=“linkbox”><a href="${resource.downloadLink}" target="_blank"><div id="download"><h2>点击下载</h2></div></a><a href="${resource.website}" target="_blank"><div id="download"><h2>前往官网</h2></div></a>`;
         resourcesList.appendChild(div);
     });
 }
@@ -30,7 +32,7 @@ function loadResources() {
 // 搜索功能
 function searchResources(categoryfilter) {
     const searchQuery = document.getElementById("search").value.toLowerCase();
-    const queryedResources = resources.filter(resource => resource.name.toLowerCase().includes(searchQuery));
+    const queryedResources = resources.filter(resource => resource.displayName.toLowerCase().includes(searchQuery));
     const resourcesList = document.getElementById("resources-list");
     var filteredResources, categoryName;
     if (categoryfilter == "category") {
@@ -49,20 +51,22 @@ function searchResources(categoryfilter) {
         } else {
             imagesrc = "image/nullicon.png";
         }
-        div.innerHTML = `<a href="resource.html?id=${resource.id}"><div id="content_pic"><img src="${imagesrc}" alt="${resource.name}" width=100px height=100px></div><div id="content_info"><h1>${resource.name}</h1></p>${space}${resource.description} </div></a><div id=“linkbox”><a href="${resource.downloadLink}"><div id="download"><h2>点击下载</h2></div></a><a href="${resource.downloadLink}"><div id="download"><h2>前往官网</h2></div></a>`;
+        div.innerHTML = `<a href="resource.html?id=${resource.id}" target="_blank"><div id="content_pic"><img src="${imagesrc}" alt="${resource.displayName}" width=100px height=100px></div><div id="content_info"><h1>${resource.displayName}</h1></p>${space}${resource.detailTitle} </div></a><div id=“linkbox”><a href="${resource.downloadLink}" target="_blank"><div id="download"><h2>点击下载</h2></div></a><a href="${resource.website}" target="_blank"><div id="download"><h2>前往官网</h2></div></a>`;
         resourcesList.appendChild(div);
     });
 }
 
-// 页面加载时调用相应的函数
-if (document.getElementById("resources-list")) {
-    if (document.getElementById("category-name")) {
-        loadCategoryResources();
-    } else {
-        loadResources();
+getJSON().then(() => {
+    // 页面加载时调用相应的函数
+    if (document.getElementById("resources-list")) {
+        if (document.getElementById("category-name")) {
+            loadCategoryResources();
+        } else {
+            loadResources();
+        }
     }
-}
 
-if (document.getElementById("resource-name")) {
-    loadResourceDetail();
-}
+    if (document.getElementById("resource-name")) {
+        loadResourceDetail();
+    }
+});
